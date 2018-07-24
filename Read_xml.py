c6f7380f4971a38c87858e4490cae9520d26cf7d
brian220 <b608390@gmail.com>
2018-07-24 09:46:54 +0800

    change the way the annotation class store the variable by dictionary

--- Read_xml.py
+++ Read_xml.py
@@ -1,42 +1,45 @@
 import xml.etree.cElementTree as ET
 import os.path
+import math
+"""
+This program is used for read an annotation part and store in class Annotation
+border = [0,0,0,0] #A 4 - index list [leftmost, rightmost, upmost, downmost]
+"""
 class Annotation():
-  coordinateX = []
-  coordinateY = []
-  border = [0,0,0,0] #A 4 - index list [leftmost, rightmost, upmost, downmost]
-  box = [0,0]# A 2 - index list [width, length]
-  def __init__(self, index, name):
-    self.index = index
-    self.name = name
+  def __init__(self, **kwargs, coordinateX, coordinateY, border):
+    for key in kwargs:
+        self.__dict__[key].get(key)
+    self.coordinateX = coordinateX
+    self.coordinateY = coordinateY
+    self.border = border
   def compute_border(self):
-    self.border[0] = self.coordinateX.min()
-    self.border[1] = self.coordinateX.max()
-    self.border[2] = self.coordinateY.min()
-    self.border[3] = self.coordinateY.max()
-  def compute_box(self):
-    self.box[0] = self.border[1] - self.border[0]
-    self.box[1] = self.border[3] - self.border[2]
+    self.border[0] = min(self.coordinateX)
+    self.border[1] = max(self.coordinateX)
+    self.border[2] = min(self.coordinateY)
+    self.border[3] = max(self.coordinateY)
 
-
-#file_name = "patient_004_node_4.xml"
+file_name = "patient_004_node_4.xml"
 def read_xml(file_name):
   annlist = []
   tree = ET.ElementTree(file = file_name)
-  index = 0
-
-  for elem in tree.iter("Annotation"):
-     ann = Annotation(index, elem.get('Name'))
-     print (index)
+  for index, elem in enumerate(tree.iter("Annotation")):
+     ann_dict = {"name" : "Annotation" + str(index) ,"type" :  elem.get("Type"), "index" :  index,"part" : elem.get("PartOfGroup")}
+     ann = Annotation(index, ann_dict, [], [], [0,0,0,0])
      for coor_elem in elem.iter("Coordinate"):
        X = coor_elem.get('X')
        Y = coor_elem.get('Y')
-       ann.coordinateX.append(X)
-       ann.coordinateY.append(Y)
+       print(index)
+       ann.coordinateX.append(int(float(X)))
+       ann.coordinateY.append(int(float(Y)))
      annlist.append(ann)
-     index += 1
+     print (annlist[index].coordinateX)
 
   #compute the annotation block
   for ann in annlist:
-    ann.compute_border()
-    ann.compute_box()
+    print (ann.coordinateX)
+    if ann.type != "None":
+      ann.compute_border()
+      ann.compute_box()
   return annlist
+
+read_xml("patient_004_node_4.xml")
