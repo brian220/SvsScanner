2c1a2de6a415e61d02d4dcec5cc4ea9389f7209e
brian220 <b608390@gmail.com>
2018-07-24 10:38:14 +0800

    change the aurgument in __init__, and add some comments

--- Read_xml.py
+++ Read_xml.py
@@ -6,12 +6,13 @@ This program is used for read an annotation part and store in class Annotation
 border = [0,0,0,0] #A 4 - index list [leftmost, rightmost, upmost, downmost]
 """
 class Annotation():
-  def __init__(self, **kwargs, coordinateX, coordinateY, border):
+  def __init__(self, **kwargs):
+     #store the ann information in xml tree
     for key in kwargs:
-        self.__dict__[key].get(key)
-    self.coordinateX = coordinateX
-    self.coordinateY = coordinateY
-    self.border = border
+        self.__dict__[key] = kwargs.get(key)
+    self.coordinateX = []
+    self.coordinateY = []
+    self.border = [0,0,0,0]
   def compute_border(self):
     self.border[0] = min(self.coordinateX)
     self.border[1] = max(self.coordinateX)
@@ -23,23 +24,21 @@ def read_xml(file_name):
   annlist = []
   tree = ET.ElementTree(file = file_name)
   for index, elem in enumerate(tree.iter("Annotation")):
-     ann_dict = {"name" : "Annotation" + str(index) ,"type" :  elem.get("Type"), "index" :  index,"part" : elem.get("PartOfGroup")}
-     ann = Annotation(index, ann_dict, [], [], [0,0,0,0])
-     for coor_elem in elem.iter("Coordinate"):
-       X = coor_elem.get('X')
-       Y = coor_elem.get('Y')
-       print(index)
-       ann.coordinateX.append(int(float(X)))
-       ann.coordinateY.append(int(float(Y)))
-     annlist.append(ann)
-     print (annlist[index].coordinateX)
+     if(elem.get("Type") != "None"):
+       ann = Annotation(**{"name" : elem.get("Name"), "type" : elem.get("Type"), "index" : index, "part" : elem.get("PartOfGroup")})
+       for coor_elem in elem.iter("Coordinate"):
+         X = coor_elem.get("X")
+         Y = coor_elem.get("Y")
+         ann.coordinateX.append(int(float(X)))
+         ann.coordinateY.append(int(float(Y)))
+       annlist.append(ann)
+       print (ann.coordinateX)
 
+  print(len(annlist))
   #compute the annotation block
   for ann in annlist:
     print (ann.coordinateX)
-    if ann.type != "None":
-      ann.compute_border()
-      ann.compute_box()
+    ann.compute_border()
   return annlist
 
 read_xml("patient_004_node_4.xml")
