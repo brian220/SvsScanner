1e6a7149cd9c4cb4f5c78a83ad21ffb8ad2c4eec
brian220 <b608390@gmail.com>
2018-08-08 16:55:49 +0800

    devide the big function to little functions

--- ScanAnnotation.py
+++ ScanAnnotation.py
@@ -7,8 +7,8 @@ source: http://alienryderflex.com/polygon/
 """
 
 import sys
-#sys.path.append("../Read_xml.py")
-#import Read_xml
+sys.path.append("../Read_xml.py")
+import Read_xml
 import openslide
 from openslide import open_slide # http://openslide.org/api/python/
 import numpy as np
@@ -38,6 +38,13 @@ def pointLieRight(testPoint, polyPoint1, polyPoint2):
         lieRight = True
   return lieRight
 
+def pointInPoly(testPoint, polyPointGroup):
+  inPoly = False
+  for i in range(0, len(polyPointGroup) - 1):
+      if (pointLieRight(testPoint, polyPointGroup[i], polyPointGroup[i+1])):
+        inPoly = not inPoly
+  return inPoly
+
 def groupXY(polyXGroup, polyYGroup):
   polyPointGroup = []
   for i in range(0, len(polyXGroup)):
@@ -49,44 +56,42 @@ def dealWithPolyGroup(polyGroup):
   polyGroup.append(polyGroup[0])
   return polyGroup
 
-def pointInPoly(testPoint, polyXGroup, polyYGroup):
-  inPoly = False
-  dealWithPolyGroup(polyXGroup)
-  dealWithPolyGroup(polyYGroup)
-  polyPointGroup = groupXY(polyXGroup, polyYGroup)
-  for i in range(0, len(polyPointGroup) - 1):
-      if (pointLieRight(testPoint, polyPointGroup[i], polyPointGroup[i+1])):
-        inPoly = not inPoly
-  return inPoly
+def getPolyPoints(ann):
+   polyXGroup = dealWithPolyGroup(ann.coordinateX)
+   polyYGroup = dealWithPolyGroup(ann.coordinateY)
+   polyPointGroup = groupXY(polyXGroup, polyYGroup)
 
-
-print (pointInPoly(point(3,3), polyXGroup, polyYGroup))
-print (pointInPoly(point(3,10), polyXGroup, polyYGroup))
-print (pointInPoly(point(10,10), polyXGroup, polyYGroup))
-print (pointInPoly(point(4, 8), polyXGroup, polyYGroup))
-"""
-def ann_detect():
-  annlist = Read_xml.read_xml("patient_004_node_4.xml")
-  polyXGroup = annlist[0].coordinateX
-  polyYGroup = annlist[0].coordinateY
-  # find out if each point is in the polygon, and use a tag to record in the dictionary
-  patch_dict = {}
-  for  ann in annlist:
+# find out if each point is in the polygon, and use a tag to record in the dictionary
+def tagInPoly(ann, polyPointGroup):
+    pointInPolytag = {}
     for i in range(ann.border[0], ann.border[1], patch_size):
       for j in range(ann.border[2], ann.border[3], patch_size):
-        if inpoly(i, j, polyXGroup, polyYGroup):
-          patch_dict.update({(i, j) : True})
+        if inpoly(point(i, j), polyPointGroup):
+          pointInPolytag.update({(i, j) : True})
         else:
-          patch_dict.update({(i, j) : False})
+          pointInPolytag.update({(i, j) : False})
+    return pointInPolytag
 
- # save the region that is all in the polygon
-   index = 0
-   for ann in annlist:
-     for i in range(ann.border[0], ann.border[1], patch_size):
-       for j in range(ann.border[2], ann.border[3], patch_size):
-         if patch_dict.get((i, j)) && patch_dict.get((i + patch_size, j)) &&
-            patch_dict.get((i , j + patch_size)) && patch_dict.get((i + patch_size, j + patch_size)):
-            img  = open.read_region((i,j), 0 , (patch_size, patch_size))
-            img.save(dir_folder + str(index) + ".png")
-            index += 1
-"""
+def fourPointsInPoly(i, j, pointInPolytag):
+  inPoly = False
+  if patch_dict.get((i, j)) && patch_dict.get((i + patch_size, j)) &&
+     patch_dict.get((i , j + patch_size)) && patch_dict.get((i + patch_size, j + patch_size)):
+     inpoly = True
+  return inPoly
+
+# save the region that is all in the polygon
+def saveInsideRegions():
+  imgIndex = 0
+    for i in range(ann.border[0], ann.border[1], patch_size):
+      for j in range(ann.border[2], ann.border[3], patch_size):
+        if fourPointsInPoly(i, j, pointInPolytag):
+           img  = open.read_region((i,j), 0 , (patch_size, patch_size))
+           img.save(dir_folder + str(imgIndex) + ".png")
+           imgIndex += 1
+
+def scanAnnotations():
+  annlist = Read_xml.read_xml("patient_004_node_4.xml")
+  for  ann in annlist:
+      polyPointGroup = getPolyPoints(ann)
+      pointInPolytag = tagInPoly(ann, polyPointGroup)
+      saveInsideRegions()
