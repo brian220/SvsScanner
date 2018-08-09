98351eb7098eeecc9e0f4e4ba9144dee21281398
brian220 <b608390@gmail.com>
2018-08-09 11:53:59 +0800

    remove some nessarary comments

--- ShowAnnPart.py
+++ ShowAnnPart.py
@@ -1,18 +1,6 @@
 import matplotlib.pyplot as plt
 #class showAnnPart(object):
-"""
-pts = [(162, 137), (211, 158), (89, 133)]
-ax = plt.subplot(111)
-triangle = plt.Polygon(pts, fill=None, edgecolor='r')
-ax.add_patch(triangle)
-pts2 = [(123,15), (121,35), (225,215)]
-triangle2 = plt.Polygon(pts2, fill=None, edgecolor='b')
-ax.add_patch(triangle2)
 
-ax.plot()
-
-plt.show()
-"""
 def addPolygonGraph(polyXGroup, polyYGroup):
   polygonPoints = []
   for i in range(0, len(polyXGroup)):
